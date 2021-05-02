97468a44e0c8e99a6778af44fac2e9bb8160bbe0
Massao <lmassao@yahoo.com.br>
2021-05-02 17:31:14 -0300

    Atualiza o fornecedor-api para buscar configurações no Config Server; Renomeia o repositorio devido a conflito com spring

--- fornecedor-api/src/main/java/br/com/massao/floricultura/microservices/fornecedor/service/InfoServiceImpl.java
+++ fornecedor-api/src/main/java/br/com/massao/floricultura/microservices/fornecedor/service/InfoServiceImpl.java
@@ -1,7 +1,7 @@
 package br.com.massao.floricultura.microservices.fornecedor.service;
 
 import br.com.massao.floricultura.microservices.fornecedor.model.InfoFornecedor;
-import br.com.massao.floricultura.microservices.fornecedor.repository.InfoRepository;
+import br.com.massao.floricultura.microservices.fornecedor.repository.InfoFornecedorRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class InfoServiceImpl implements InfoService {
     @Autowired
-    private InfoRepository infoRepository;
+    private InfoFornecedorRepository infoRepository;
 
     @Override
     public InfoFornecedor getInfoPorEstado(String estado) {
